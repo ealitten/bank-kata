01b4e2dee30d896ac5293015fe2c7a149c5bab31
Ed <ea.litten@gmail.com>
2018-02-26 15:13:27 +0000

    Better test for initialize

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,8 +4,16 @@ describe Account do
 
   subject(:account) { described_class.new }
 
-  it 'should start with a balance of zero' do
+  describe '#initialize' do
+    it 'should start with a balance of zero by default' do
+      account = Account.new
     expect(account.balance).to eq 0
+  end
+
+    it 'should start with starting balance if supplied' do
+      account = Account.new(123)
+      expect(account.balance).to eq 123
+    end
   end
 
   describe '#deposit' do
