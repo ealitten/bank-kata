2baa8c7eb5f98ddcef10c00e00ffb1eb442b8de0
Ed <ea.litten@gmail.com>
2018-02-26 16:49:39 +0000

    Update tests for creating transaction

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,10 +1,12 @@
 require './account'
+require 'timecop'
 
 describe Account do
 
+  starting_balance = 750
   let(:transaction) { double :transaction }
   let(:transaction_class) { double :transaction_class, new: transaction }
-  subject(:account) { described_class.new(750, transaction_class) }
+  subject(:account) { described_class.new(starting_balance, transaction_class) }
 
   describe '#initialize' do
     it 'should start with a balance of zero by default' do
@@ -19,14 +21,21 @@ describe Account do
   end
 
   describe '#deposit' do
+
+  before { Timecop.freeze }
+  after { Timecop.return }
+
     it 'should add specified amount to balance' do
       expect{account.deposit(500)}.to change {account.balance}.by(500)
     end
 
-    it 'should create a new transaction' do
-      account.withdraw(500)
-      expect(transaction_class).to have_received(:new)
+     
+    it 'should create a new transaction with amount, new_balance, date' do
+      amount = 500
+      account.withdraw(amount)
+      expect(transaction_class).to have_received(:new).with(-amount, starting_balance - amount, DateTime.now)
     end
+    
 
     it 'should add deposit to transaction history' do
       account.deposit(500)
@@ -36,14 +45,19 @@ describe Account do
 
   describe '#withdraw' do
 
+  before { Timecop.freeze }
+  after { Timecop.return }
+
     it 'should remove specified amount from balance' do
-      account.withdraw(500)
-      expect(account.balance).to eq 250
+      amount = 500
+      account.withdraw(amount)
+      expect(account.balance).to eq starting_balance - amount
     end
 
-    it 'should create a new transaction' do
-      account.withdraw(500)
-      expect(transaction_class).to have_received(:new)
+    it 'should create a new transaction with amount, new_balance, date' do
+      amount = 500
+      account.deposit(amount)
+      expect(transaction_class).to have_received(:new).with(amount, starting_balance + amount, DateTime.now)
     end
 
     it 'should add withdrawal to transaction history' do
