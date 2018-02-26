8e07465b90b10924c36b9613559f99149c3b000a
Ed <ea.litten@gmail.com>
2018-02-26 16:12:18 +0000

    Add basic tests for transaction

---
+++ spec/transaction_spec.rb
@@ -0,0 +1,24 @@
+require './transaction'
+require 'timecop'
+
+describe Transaction do
+
+  Timecop.freeze do
+
+    subject(:transaction) { described_class.new(1000, 1500, DateTime.now) }
+
+    it 'should store amount' do
+      expect(transaction.amount).to eq 1000
+    end
+
+    it 'should store new balance' do
+      expect(transaction.new_balance).to eq 1500
+    end
+
+    it 'should store its creation date' do
+      expect(transaction.date).to eq DateTime.now
+    end
+
+  end
+
+end
