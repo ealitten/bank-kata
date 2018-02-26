7034ff23b3044b53690c002484124e7043c80700
Ed <ea.litten@gmail.com>
2018-02-26 18:08:43 +0000

    Refactor printer

--- printer.rb
+++ printer.rb
@@ -1,12 +1,15 @@
 class Printer
 
   def print_transaction_history(transactions)
-    output = ""
-    output << "date || credit || debit || balance\n"
-    transactions.each { |t|
-      output << "#{t.date.strftime('%d/%m/%Y')} || #{sprintf('%.2f', t.amount) if t.amount > 0} || #{sprintf('%.2f', t.amount) if t.amount < 0} || #{sprintf('%.2f', t.new_balance)}\n"
-    }
+    print_header
+    output = transactions.map { |t|
+      "#{t.date.strftime('%d/%m/%Y')} || #{sprintf('%.2f', t.amount) if t.amount > 0} || #{sprintf('%.2f', t.amount) if t.amount < 0} || #{sprintf('%.2f', t.new_balance)}"
+               }.join {'\n'}
     print output
+  end
+
+  def print_header
+    puts "date || credit || debit || balance"
   end
 
 end
