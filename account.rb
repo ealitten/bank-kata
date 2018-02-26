711788ed8583e7e964a98d84ecde65dbbe4277df
Ed <ea.litten@gmail.com>
2018-02-26 15:31:36 +0000

    Add empty transaction class & inject it into Account on initialise

--- account.rb
+++ account.rb
@@ -1,8 +1,11 @@
+require './transaction'
+
 class Account
 
-  def initialize(starting_balance = 0)
+  def initialize(starting_balance = 0, transaction_class = Transaction)
     @balance = starting_balance
     @transactions = []
+    @transaction_class = transaction_class
   end
 
   def balance
