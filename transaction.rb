74e021a7a0aa49bfe54e716705303c561669545d
Ed <ea.litten@gmail.com>
2018-02-26 16:22:49 +0000

    Transaction passes tests

--- transaction.rb
+++ transaction.rb
@@ -1,2 +1,21 @@
 class Transaction
+
+  def initialize(amount, new_balance, date)
+    @amount = amount
+    @new_balance = new_balance
+    @date = date
+  end
+
+  def amount
+    @amount.dup
+  end
+
+  def new_balance
+    @new_balance.dup
+  end
+
+  def date
+    @date.dup
+  end
+
 end
